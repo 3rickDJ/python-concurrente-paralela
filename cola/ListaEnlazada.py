a501f017da56758bb2b09ace43acc81c850d413c
3rickDJ <erickdejesusmolina@gmail.com>
2023-08-24 08:06:34 -0600

    add: imprime lista enlazada

--- cola/ListaEnlazada.py
+++ cola/ListaEnlazada.py
@@ -6,12 +6,16 @@ class Nodo:
     def __str__(self):
         return str(self.carga)
 
+def imprimeLista(nodo):
+    while nodo:
+        print(nodo);
+        nodo = nodo.siguiente
+    print()
+
 if __name__ == '__main__':
     nodo1 = Nodo(1)
     nodo2 = Nodo(2)
     nodo3 = Nodo(3)
     nodo1.siguiente = nodo2
     nodo2.siguiente = nodo3
-    print(nodo1)
-    print(nodo1.siguiente)
-    print(nodo1.siguiente.siguiente)
+    imprimeLista(nodo1)
